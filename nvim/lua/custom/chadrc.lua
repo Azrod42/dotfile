deed04b3f0166f944a50cb855e31121a8dd34c20
tsorabel <tsorabel@snc-tom.home>
2023-10-13 05:58:10 -0400

    refactor: nvim dotfile

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -1,7 +1,11 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'catppuccin' }
+M.ui = {
+    theme_toggle = "palenight",
+    theme = "palenight",
+    transparency = true
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -3,4 +3,49 @@ local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>")
 keymap.set("n", "x", "_x")
+-- Remove BG
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.updatetime = 50
+
+vim.keymap.set("v", 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", 'K', ":m '>-2<CR>gv=gv")
+vim.keymap.set("n", 'C-d', "<C-d>zz")
+vim.keymap.set("n", 'C-u', "<C-u>zz")
+vim.keymap.set("n", 'C-{', "<C-{>zz")
+vim.keymap.set("n", 'C-}', "<C-}>zz")
+vim.keymap.set('n', "n", "nzzzv")
+vim.keymap.set('n', "N", "Nzzzv")
+vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set({"n", "v"}, "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set("n", "<leader>X", "!chmod +x %<CR>")
+vim.keymap.set("n", "<C-F>", ":silent !tmux neww ~/.config/flash<CR>")
+vim.keymap.set("n", "<C-P>", ":silent !tmux neww ~/.config/cheat_sheet<CR>")
 
